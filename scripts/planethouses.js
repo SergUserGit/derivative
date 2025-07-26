431ab6e45c94066c423a585c7a27441eb8d7b238
Sergii Podriezienko <podreenko1986@gmail.com>
2025-07-26 10:55:09 +0300

    add: prepare for development
    add: prepare for development

--- scripts/planethouses.js
+++ scripts/planethouses.js
@@ -1,8 +1,12 @@
 const buttonAsp = document.querySelector(".tract_button");
 
+const planetName = document.querySelector("#aspect-sun");
+const houseName = document.querySelector("#number-house");
+
 buttonAsp.addEventListener("click", onClickBtnAsp);
 
-function onClickBtnAsp()
-{
-  alert("!!!");
+function onClickBtnAsp() {
+  let planetValue = planetName.options[planetName.value - 1].textContent;
+  let houseValue = houseName.options[houseName.value - 1].textContent;
+  alert(planetValue + " - " + houseValue);
 }
