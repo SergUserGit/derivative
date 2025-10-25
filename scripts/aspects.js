62e16532dde6d5bb53babf83888b25ae43552284
Sergii Podriezienko <podreenko1986@gmail.com>
2025-10-25 12:05:04 +0300

    add: test_module
    add: test_module

--- scripts/aspects.js
+++ scripts/aspects.js
@@ -1,7 +1,56 @@
+import {
+  getKeysOfPlanets,
+  getKeysOfHouses,
+  getItnerpretation,
+} from "./test_module.js";
+
 const aspButton = document.querySelector(".aspet-button");
 
 aspButton.addEventListener("click", onClickButtonAspect);
 
 function onClickButtonAspect() {
-  alert("click !!!");
+  const planetValue = "Венера";
+  const houseValue = "12";
+
+  const keysOfPlanet = getKeysOfPlanets();
+
+  const keysOfHouses = getKeysOfHouses();
+
+  const foundArray = keysOfPlanet.find(
+    (element) => element.planet === planetValue
+  );
+
+  const foundArrayHouses = keysOfHouses.find(
+    (element) => element.planet === houseValue
+  );
+
+  if (foundArray !== undefined && foundArrayHouses !== undefined) {
+    const arrayOfInterpCurrent = arrayOfInterp(
+      foundArray.planetArray,
+      foundArrayHouses.planetArray
+    );
+
+    console.log(arrayOfInterpCurrent);
+  }
+}
+
+function arrayOfInterp(planetArray, housesArray) {
+  let curArray = [];
+
+  for (let i = 0; i < planetArray.length; i += 1) {
+    for (let b = 0; b < housesArray.length; b += 1) {
+      const strOne = planetArray[i];
+      const strTwo = housesArray[b];
+
+      const intert = getItnerpretation(strOne, strTwo);
+
+      if (intert === "") {
+        continue;
+      }
+
+      curArray.push(intert);
+    }
+  }
+
+  return curArray;
 }
